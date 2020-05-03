397d8a1df9391a8ad1a1359730cab818bbc137af
Vitor Leonel <falecom@vitorleonel.com>
2020-05-03 02:57:13 -0300

    sales and products chart

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "react-native";
+
+import DashboardScreen from "./src/screens/Dashboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <DashboardScreen />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Dashboard/index.js
@@ -0,0 +1,259 @@
+import React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  ScrollView,
+  Image,
+} from "react-native";
+import Constants from "expo-constants";
+
+import { Block, Text } from "galio-framework";
+import {
+  VictoryGroup,
+  VictoryBar,
+  VictoryLine,
+  VictoryChart,
+  VictoryAxis,
+} from "victory-native";
+import { Svg, Defs, LinearGradient, Stop, Ellipse } from "react-native-svg";
+
+const { width, height } = Dimensions.get("window");
+
+export default function Dashboard() {
+  return (
+    <ImageBackground
+      source={require("../../images/profile-screen-bg.png")}
+      style={styles.container}
+      imageStyle={styles.imageContainer}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Block shadow style={[styles.card, styles.profile]}>
+          <Image
+            source={{
+              uri:
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+            }}
+            style={styles.profileImage}
+          />
+
+          <Block alignSelf="center" style={{ marginBottom: 24 }}>
+            <Text
+              size={24}
+              color="#32325D"
+              bold
+              style={{ textAlign: "center" }}
+            >
+              Mariana Silveira
+            </Text>
+            <Text size={16} color="#32325D" style={{ textAlign: "center" }}>
+              Ribeirão Preto - SP
+            </Text>
+          </Block>
+
+          <Block row style={[styles.profileCardGroup, { marginBottom: 8 }]}>
+            <Block flex middle style={[styles.profileCard]}>
+              <Text color="white" size={20} bold style={styles.textCenter}>
+                R$ 25,00
+              </Text>
+              <Text color="white" style={[styles.textCenter, { marginTop: 4 }]}>
+                Comissão à receber
+              </Text>
+            </Block>
+
+            <Block flex middle style={[styles.profileCard]}>
+              <Text color="white" size={20} bold style={styles.textCenter}>
+                25
+              </Text>
+              <Text color="white" style={[styles.textCenter, { marginTop: 4 }]}>
+                Produtos na vitrine
+              </Text>
+            </Block>
+          </Block>
+
+          <Block row style={styles.profileCardGroup}>
+            <Block
+              flex
+              middle
+              style={[styles.profileCard, styles.profileCardBlue]}
+            >
+              <Text color="white" size={20} bold style={styles.textCenter}>
+                115
+              </Text>
+              <Text color="white" style={[styles.textCenter, { marginTop: 4 }]}>
+                Total de vendas
+              </Text>
+            </Block>
+
+            <Block
+              flex
+              middle
+              style={[styles.profileCard, styles.profileCardBlue]}
+            >
+              <Text color="white" size={20} bold style={styles.textCenter}>
+                R$ 425,00
+              </Text>
+              <Text color="white" style={[styles.textCenter, { marginTop: 4 }]}>
+                Total de comissão
+              </Text>
+            </Block>
+          </Block>
+        </Block>
+
+        <Block shadow style={[styles.card, { padding: 0 }]}>
+          <Text color="#525F7F" bold size={16} style={{ padding: 16 }}>
+            Relatorio de vendas últimos 6 meses
+          </Text>
+
+          <VictoryChart
+            width={width - 40}
+            padding={{ bottom: 48, left: 32, right: 32, top: 48 }}
+          >
+            <VictoryAxis
+              style={{
+                axis: { stroke: "none" },
+                tickLabels: {
+                  stroke: "#999FBF",
+                  fontWeight: 100,
+                },
+              }}
+              crossAxis
+            />
+
+            <VictoryBar
+              barWidth={10}
+              cornerRadius={{ top: 5, bottom: 5 }}
+              alignment="start"
+              style={{
+                data: { fill: "#FF5959" },
+              }}
+              labels={(value) => `${parseFloat(value.datum.y)}k`}
+              data={[
+                { x: "Jan", y: 1 },
+                { x: "Fev", y: 7.5 },
+                { x: "Mar", y: 10.0 },
+                { x: "Abr", y: 3.0 },
+                { x: "Mai", y: 16.0 },
+                { x: "Jun", y: 3.0 },
+              ]}
+            />
+          </VictoryChart>
+        </Block>
+
+        <Block shadow style={styles.card}>
+          <Text color="#525F7F" bold size={16}>
+            Estoque x vendas feitas
+          </Text>
+        </Block>
+
+        <Block shadow style={[styles.card, { padding: 0 }]}>
+          <Text color="#525F7F" bold size={16} style={{ padding: 16 }}>
+            Produtos ativos
+          </Text>
+
+          <VictoryGroup
+            width={width - 40}
+            domainPadding={{ y: 32, x: 16 }}
+            padding={{ bottom: 48, left: 16, right: 16, top: 24 }}
+          >
+            <Defs>
+              <LinearGradient id="grad" x1="0%" x2="0%" y1="0%" y2="100%">
+                <Stop offset="0%" stopColor="#109CF1" stopOpacity="0.2" />
+                <Stop offset="100%" stopColor="#109CF1" stopOpacity="0.005" />
+              </LinearGradient>
+            </Defs>
+
+            <VictoryChart>
+              <VictoryAxis
+                style={{
+                  axis: { stroke: "none" },
+                  tickLabels: {
+                    stroke: "#999FBF",
+                    fontWeight: 100,
+                  },
+                }}
+                crossAxis
+              />
+
+              <VictoryLine
+                style={{
+                  data: {
+                    stroke: "#109CF1",
+                    fill: "url(#grad)",
+                  },
+                }}
+                labels={(value) => value.datum.y}
+                data={[
+                  { x: "Jan", y: 10 },
+                  { x: "Fev", y: 34 },
+                  { x: "Mar", y: 20 },
+                  { x: "Abr", y: 29 },
+                  { x: "Mai", y: 16 },
+                  { x: "Jun", y: 10 },
+                ]}
+              />
+            </VictoryChart>
+          </VictoryGroup>
+        </Block>
+      </ScrollView>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height,
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+
+  imageContainer: {
+    width,
+    height: height / 1.25,
+  },
+
+  card: {
+    padding: 16,
+    backgroundColor: "#ffffff",
+    marginBottom: 24,
+    marginHorizontal: 20,
+    borderRadius: 6,
+    elevation: 2,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#f1f2f6",
+  },
+
+  profile: {
+    marginTop: 128,
+  },
+
+  profileImage: {
+    width: 112,
+    height: 112,
+    borderRadius: 64,
+    alignSelf: "center",
+    marginTop: -72,
+    marginBottom: 16,
+  },
+
+  profileCardGroup: {
+    margin: -8,
+  },
+
+  profileCard: {
+    padding: 12,
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#2DCE89",
+  },
+
+  profileCardBlue: {
+    backgroundColor: "#4CB5FF",
+  },
+
+  // utils
+  textCenter: {
+    textAlign: "center",
+  },
+});
